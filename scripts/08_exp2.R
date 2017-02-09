9ab2fa2af349efc9c42d55d218ed391b987e2f8a
Frederik Hjorth <frederikhjorth@gmail.com>
2017-02-09 08:36:28 +0100

    ri

--- scripts/08_exp2.R
+++ scripts/08_exp2.R
@@ -3,6 +3,22 @@ setwd("~/GitHub/lqrps17")
 require(readr)
 require(dplyr)
 require(broom)
+require(ggplot2)
+require(ri)
+
+# RANDOMIZATION INFERENCE
+
+set.seed(123456)
+y <- rnorm(500,100,15)
+Z <- sample(0:1,500,replace=T)
+cluster <- rep(1:5,each=100)
+perms <- genperms(Z, clustvar=cluster) # all possible permutations
+probs <- genprobexact(Z, clustvar=cluster) # probability of treatment
+ate <- estate(y,Z,prob=probs) # estimate the ATE
+## Conduct Sharp Null Hypothesis Test of Zero Effect for Each Unit
+Ys <- genouts(y,Z,ate=0) # generate potential outcomes under sharp null of no effect
+distout <- gendist(Ys,perms, prob=probs) # generate sampling dist. under sharp null
+dispdist(distout, ate) # display characteristics of sampling dist. for inference
 
 #indlæs data
 ggd<-read_csv("data/8_gg.csv")
